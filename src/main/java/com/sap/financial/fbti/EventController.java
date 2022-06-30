f55eedd680c4fba9731696e6743a18514f280418
Guo, Yong Kang <yong.kang.guo@sap.com>
2022-06-30 16:51:32 +0800

    Externalize the destination name

--- src/main/java/com/sap/financial/fbti/EventController.java
+++ src/main/java/com/sap/financial/fbti/EventController.java
@@ -1,30 +1,25 @@
 package com.sap.financial.fbti;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sap.cloud.sdk.cloudplatform.connectivity.DestinationAccessor;
+import com.sap.cloud.sdk.cloudplatform.connectivity.HttpClientAccessor;
+import com.sap.cloud.sdk.cloudplatform.connectivity.HttpDestination;
 import io.cloudevents.CloudEvent;
 import io.cloudevents.core.format.EventFormat;
 import io.cloudevents.core.provider.EventFormatProvider;
 import io.cloudevents.jackson.JsonFormat;
-import io.cloudevents.rw.CloudEventRWException;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
+import org.apache.http.client.HttpClient;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
 
 @RestController
 @AllArgsConstructor
 @Slf4j
 public class EventController {
+
+    private final DestinationConfigurationProperties destinationConfigurationProperties;
     private static final EventFormat CE_JSON_FORMATTER =
             EventFormatProvider.getInstance().resolveFormat(JsonFormat.CONTENT_TYPE);
 
@@ -32,6 +27,14 @@ public class EventController {
     public ResponseEntity<String> handleEvent(@RequestBody CloudEvent event) {
         String request = stringify(event);
         log.debug("Event received:\n" + request);
+
+        final String destinationName = destinationConfigurationProperties.getName();
+        HttpDestination destination = DestinationAccessor.getDestination(destinationName).asHttp();
+        log.debug("Destination '{}' is retrieved", destinationName);
+        log.debug("Destination URI: {}", destination.getUri());
+
+        HttpClient httpClient = HttpClientAccessor.getHttpClient(destination);
+
 
         return ResponseEntity.noContent().build();
     }
